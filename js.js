350064fec8e0e05221d552a6ed056e066bbb35ac
Pablo Diaz Mazzaro <diazmazzaro@gmail.com>
2020-04-19 21:18:08 -0300

    new features

--- js.js
+++ js.js
@@ -14,8 +14,22 @@ $(document).ready(function(){
   window.selectByMatterState = function(state, clear=true){
     if(clear){
       $('.element-card[data-selected]').removeAttr('data-selected');
+      $('.element-card[data-disabled]').removeAttr('data-disabled');
+      
     }
-    $('.element-card[data-matter-states="' + state+'"]').attr('data-selected', 'true');
+    if(state != 'xXxXx'){
+      $('.element-card[data-matter-states="' + state+'"]').attr('data-selected', 'true');
+      $('.element-card:not([data-matter-states="' + state+'"])').attr('data-disabled', 'true');
+    }
+  }
+  
+  window.selectByGroup = function(state, clear=true){
+    if(clear){
+      $('.element-card[data-selected]').removeAttr('data-selected');
+       $('.element-card[data-disabled]').removeAttr('data-disabled');
+    }
+    $('.element-card[data-group="' + state+'"]').attr('data-selected', 'true');
+    $('.element-card:not([data-group="' + state+'"])').attr('data-disabled', 'true');
   }
   
 });
